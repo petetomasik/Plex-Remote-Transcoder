244d2e28c4f88e701609ee92fda2e2f13013faeb
Pete Tomasik <senate6268@gmail.com>
2019-05-10 16:35:26 -0500

    Continue work on api endpoints

--- api/api_server.py
+++ api/api_server.py
@@ -1,22 +1,19 @@
 import os
 import multiprocessing
 import requests
+import json
 
 from flask import Flask
 api_server = Flask(__name__)
 
 @api_server.route('/')
 def index():
-    return 'Server Works!'
+    return 'Hello World'
 
-@api_server.route('/greet')
-def say_hello():
-    return 'Hello from Server'
-
-@api_server.route('/user/<username>')
-def show_user(username):
+@api_server.route('/ping')
+def ping_respond():
     #returns the username
-    return 'Username: %s' % username
+    return str( 'Pong' )
 
 @api_server.route('/post/<int:post_id>')
 def show_post(post_id):
@@ -32,7 +29,48 @@ def get_system_load_local():
 @api_server.route('/load/remote/<host>')
 def get_system_load_remote(host):
     remote_load_result = requests.get("http://%s:5000/load/local" % host)
-    return str( remote_load_result.json() )
+    return remote_load_result.json()
+
+@api_server.route('/load/cluster')
+def get_cluster_load():
+    running_slaves = get_slave_list()
+    servers = running_slaves["servers"]
+    load_cluster = []
+    for server in servers:
+        load = ["%0.2f%%" % l for l in get_system_load_remote(server)]
+        load_cluster.append("  %15s: %s" % (server, ", ".join(load)))
+
+    return str( load_cluster )
+
+@api_server.route('/list/slaves')
+def get_slave_list():
+    #config = get_config()
+    config = {
+            "servers": {
+                "localhost", "127.0.0.1"
+                }
+            }
+
+    return config
+
+@api_server.route('/list/sessions')
+def get_session_list():
+    #config = get_config()
+    config = {
+            "sessions": {
+                "session1", "session1 details",
+                "session2", "session2 details",
+                }
+            }
+
+    return config
+
+@api_server.route('/auth_token')
+def get_auth_token():
+    config = {
+            "auth_token", "foo_bar"
+            }
+    return str( config )
 
 if __name__ == "__main__":
   api_server.run()
